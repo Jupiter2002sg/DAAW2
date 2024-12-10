0678b6bfe9702305113d6a9ca181e2df54847e38
Sarah Guellil <120458526+Jupiter2002sg@users.noreply.github.com>
2024-12-10 18:28:18 +0100

    initial commit

---
+++ _codigoEnunciado/App.jsx
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from 'react';
+import GameBoard from './components/GameBoard';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Snake Game</h1>
+            <GameBoard />
+        </div>
+    );
+}
+
+export default App;
